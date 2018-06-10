c02fc1f03ac029626be3aee5bc4c6f7da5f85fa5
Manikapavan <31723296+NMPavan@users.noreply.github.com>
2018-06-10 22:39:09 +0530

    Update Config.java

--- app/src/main/java/com/example/manikyapavan/moviesapp/Config/Config.java
+++ app/src/main/java/com/example/manikyapavan/moviesapp/Config/Config.java
@@ -1,6 +1,6 @@
 package com.example.manikyapavan.moviesapp.Config;
 
 public class Config {
-    public static final String API_KEY = "82813177051f3dab7ef745864fbfb36c";
+    public static final String API_KEY = "your_Api_key";
 }
 
